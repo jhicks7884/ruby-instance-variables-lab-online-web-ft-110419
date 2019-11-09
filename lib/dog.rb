e64d1476ed872ee73fe632a5e0bf2809f87d1aeb
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-09 22:10:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
-def name = (dogs_name)
-  @this_dogs_name = dogs_name
-end
-
-def name
-  @this_dogs_name
-end
-end
-
-lassie = dog.new
-lassie.name = "Lassie"
-
-puts lassie.name
+  def name=(dogs_name)
+     @this_dogs_name = dogs_name
+   end
+ 
+   def name
+     @this_dogs_name
+   end
+ end
+ 
+ lassie = Dog.new
+ lassie.name = "Lassie"
+ 
+ puts lassie.name
